3135f2e8c1a6ba2f6cfa5198c5de7e8d93c89c05
bhargavim0607 <68108779+bhargavim0607@users.noreply.github.com>
2021-04-28 09:40:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function check(){
     var names=document.getElementById("name");
     var email=document.getElementById("mail");
     var phone=document.getElementById("tel");
+    var numbers=/^[0-9]+$/;
     if(names.value.length===0){
         alert("Name must be filled out");
         return false;
@@ -14,7 +15,7 @@ function check(){
         alert("Enter Valid Mailid");
         return false;       
     }
-    else if(phone.value.length<10 || phone.value.length>10 || !(isNaN(phone))){
+    else if(phone.value.length<10 || phone.value.length>10 || !phone.value.match(numbers)){
         alert("Phonenumber should be 10 digit Numeric value");
         return false;
     }
